8f18c968b5d7b52d6a7fcae528e43231fafaae7f
Michel Albert <michel@albert.lu>
2017-06-12 08:25:23 +0200

    Merge fabfile for git

--- git/fabfile.py
+++ git/fabfile.py
@@ -1,7 +1,7 @@
 import fabric.api as fab
 
 fab.env.roledefs['www'] = ['exhuma@michel.albert.lu']
-INSTANCE = '2016'
+INSTANCE = '2017'
 
 
 @fab.task
@@ -11,12 +11,13 @@ def fetch_common_files():
     place for building.
     '''
     l = fab.local
-    l('cp -v ../common/*.css slides/_static')
+    l('cp -v ../common/custom.css slides/_static')
 
 
 @fab.task
 def build_slides():
     fab.execute(fetch_common_files)
+    fab.local('cp -v ../common/slidestyle.css slides/_static/htmlstyle.css')
     with fab.lcd('slides'):
         fab.local('make slides')
 
@@ -24,25 +25,28 @@ def build_slides():
 @fab.task
 def build_html():
     fab.execute(fetch_common_files)
+    fab.local('cp -v ../common/htmlstyle.css slides/_static/htmlstyle.css')
     fab.local('make html')
 
 
 @fab.task
 def build_linked():
     fab.execute(fetch_common_files)
-    fab.local('make slides html')
+    with fab.lcd('slides'):
+        fab.local('make slides html')
 
 
 @fab.task
 def clean():
-    fab.local('make clean')
+    with fab.lcd('slides'):
+        fab.local('make clean')
 
 
 @fab.task
 def serve_slides():
     fab.execute(clean)
     fab.execute(build_slides)
-    with fab.lcd('_build/slides'):
+    with fab.lcd('slides/_build/slides'):
         fab.local('python3 -m http.server')
 
 
@@ -50,7 +54,7 @@ def serve_slides():
 def serve_html():
     fab.execute(clean)
     fab.execute(build_html)
-    with fab.lcd('_build/html'):
+    with fab.lcd('slides/_build/html'):
         fab.local('python3 -m http.server')
 
 
@@ -58,19 +62,20 @@ def serve_html():
 def serve_linked():
     fab.execute(clean)
     fab.execute(build_linked)
-    with fab.lcd('_build'):
+    with fab.lcd('slides/_build'):
         fab.local('python3 -m http.server')
 
 
 @fab.task
 @fab.roles('www')
 def publish():
-    remote_folder = '/var/www/albert.lu/michel/shelf/git-%s' % INSTANCE
-    latest_folder = '/var/www/albert.lu/michel/shelf/git-latest'
-    fab.execute(build_linked)
+    remote_folder = '/var/www/html/shelf/python-advanced-%s' % INSTANCE
+    latest_folder = '/var/www/html/shelf/python-advanced-latest'
+    fab.execute(build_html)
+    fab.execute(build_slides)
     fab.run('mkdir -p %s' % remote_folder)
-    fab.put('_build/html', remote_folder)
-    fab.put('_build/slides', remote_folder)
+    fab.put('slides/_build/html', remote_folder)
+    fab.put('slides/_build/slides', remote_folder)
     with fab.settings(warn_only=True):
         fab.run('test -h {0} && rm {0}'.format(latest_folder))
     fab.run('ln -s %s %s' % (remote_folder, latest_folder))
