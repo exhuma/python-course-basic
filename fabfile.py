29f521204a54af97256e5543596f5328ec9e7fbf
Michel Albert <michel@albert.lu>
2018-12-13 07:15:21 +0100

    Upgrade to fabric 2.0

--- fabfile.py
+++ fabfile.py
@@ -1,105 +1,107 @@
-import fabric.api as fab
-from fabric.context_managers import shell_env
+from os import environ
 from os.path import abspath
 
-fab.env.roledefs['www'] = ['michel.albert.lu']
+from fabric.connection import Connection
+from invoke import task
+
+www = Connection('michel.albert.lu')
+
 INSTANCE = '2018'
 VIRTUAL_ENV = abspath('./env')
-ENVPATH = '%s/bin:$PATH' % VIRTUAL_ENV
+ENVPATH = '%s/bin:%s' % (VIRTUAL_ENV, environ['PATH'])
+CUSTOM_ENV = {'PATH': ENVPATH, 'VIRTUAL_ENV': VIRTUAL_ENV}
 
 
-@fab.task
-def fetch_common_files():
+@task
+def fetch_common_files(ctx):
     '''
     Put shared files from the "common" folder of the repository into the proper
     place for building.
     '''
-    l = fab.local
-    l('cp -v common/custom.css slides/_static')
+    ctx.run('cp -v common/custom.css slides/_static')
 
 
-@fab.task
-def build_slides():
-    fab.execute(fetch_common_files)
-    fab.local('cp -v common/slidestyle.css slides/_static/htmlstyle.css')
-    with fab.lcd('slides'):
-        with shell_env(PATH=ENVPATH, VIRTUAL_ENV=VIRTUAL_ENV):
-            fab.local('make slides')
+@task
+def build_slides(ctx):
+    fetch_common_files(ctx)
+    ctx.run('cp -v common/slidestyle.css slides/_static/htmlstyle.css')
+    with ctx.cd('slides'):
+        ctx.run('make slides', env=CUSTOM_ENV)
 
 
-@fab.task
-def build_html():
-    fab.execute(fetch_common_files)
-    fab.local('cp -v common/htmlstyle.css slides/_static/htmlstyle.css')
-    with fab.lcd('slides'):
-        with shell_env(PATH=ENVPATH, VIRTUAL_ENV=VIRTUAL_ENV):
-            fab.local('make html')
+@task
+def build_html(ctx):
+    fetch_common_files(ctx)
+    ctx.run('cp -v common/htmlstyle.css slides/_static/htmlstyle.css')
+    with ctx.cd('slides'):
+        ctx.run('export', env=CUSTOM_ENV)
+        ctx.run('make html', env=CUSTOM_ENV)
 
 
-@fab.task
-def build_linked():
-    fab.execute(fetch_common_files)
-    with fab.lcd('slides'):
-        with shell_env(PATH=ENVPATH, VIRTUAL_ENV=VIRTUAL_ENV):
-            fab.local('make slides html')
+@task
+def build_linked(ctx):
+    fetch_common_files(ctx)
+    with ctx.cd('slides'):
+        ctx.run('make slides html', env=CUSTOM_ENV)
 
 
-@fab.task
-def develop():
-    fab.local('[ -d env ] || python3 -m venv env')
-    fab.local('./env/bin/pip install hieroglyph')
-    fab.local('git submodule init')
-    fab.local('git submodule update')
+@task
+def develop(ctx):
+    ctx.run('[ -d env ] || python3 -m venv env', env=CUSTOM_ENV)
+    ctx.run('./env/bin/pip install hieroglyph', env=CUSTOM_ENV)
+    ctx.run('git submodule init', env=CUSTOM_ENV)
+    ctx.run('git submodule update', env=CUSTOM_ENV)
 
 
-@fab.task
-def clean():
-    with fab.lcd('slides'):
-        fab.local('make clean')
+@task
+def clean(ctx):
+    with ctx.cd('slides'):
+        ctx.run('/usr/bin/make clean', env=CUSTOM_ENV)
 
 
-@fab.task
-def serve_slides():
-    fab.execute(clean)
-    fab.execute(build_slides)
-    with fab.lcd('slides/_build/slides'):
-        fab.local('python3 -m http.server')
+@task
+def serve_slides(ctx):
+    clean(ctx)
+    build_slides(ctx)
+    with ctx.cd('slides/_build/slides'):
+        ctx.run('python3 -m http.server')
 
 
-@fab.task
-def serve_html():
-    fab.execute(clean)
-    fab.execute(build_html)
-    with fab.lcd('slides/_build/html'):
-        fab.local('python3 -m http.server')
+@task
+def serve_html(ctx):
+    clean(ctx)
+    build_html(ctx)
+    with ctx.cd('slides/_build/html'):
+        ctx.run('python3 -m http.server')
 
 
-@fab.task
-def serve_linked():
-    fab.execute(clean)
-    fab.execute(build_linked)
-    with fab.lcd('slides/_build'):
-        fab.local('python3 -m http.server')
+@task
+def serve_linked(ctx):
+    clean(ctx)
+    build_linked(ctx)
+    with ctx.cd('slides/_build'):
+        ctx.run('python3 -m http.server')
 
 
-@fab.task
-@fab.roles('www')
-def publish():
+@task
+def publish(ctx):
     remote_root = '/var/www/html/shelf'
     remote_folder = '%s/python-basic-%s' % (remote_root, INSTANCE)
     latest_folder = '%s/python-basic-latest' % remote_root
-    fab.execute(build_html)
-    fab.execute(build_slides)
-    fab.run('mkdir -p %s' % remote_folder)
-    fab.put('slides/_build/html', remote_folder)
-    fab.put('slides/_build/slides', remote_folder)
-    with fab.settings(warn_only=True):
-        fab.run('test -h {0} && rm {0}'.format(latest_folder))
-    fab.run('ln -s %s %s' % (remote_folder, latest_folder))
+    build_html(ctx)
+    build_slides(ctx)
+    www.run('mkdir -p %s' % remote_folder)
+    www.put('slides/_build/html', remote_folder)
+    www.put('slides/_build/slides', remote_folder)
+    try:
+        www.run('test -h {0} && rm {0}'.format(latest_folder))
+    except Exception as exc:
+        print(exc)
+    www.run('ln -s %s %s' % (remote_folder, latest_folder))
 
     pack_folder = 'python-basic-%s' % INSTANCE
-    fab.local('mkdir -p %s' % pack_folder)
-    fab.local('mv slides/_build/html slides/_build/slides %s' % pack_folder)
-    fab.local('tar czf %s.tar.gz %s' % (pack_folder, pack_folder))
-    fab.local('rm -rf %s' % pack_folder)
-    fab.put('%s.tar.gz' % pack_folder, remote_folder)
+    ctx.run('mkdir -p %s' % pack_folder)
+    ctx.run('mv slides/_build/html slides/_build/slides %s' % pack_folder)
+    ctx.run('tar czf %s.tar.gz %s' % (pack_folder, pack_folder))
+    ctx.run('rm -rf %s' % pack_folder)
+    www.put('%s.tar.gz' % pack_folder, remote_folder)
