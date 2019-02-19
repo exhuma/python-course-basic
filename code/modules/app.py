0c25413e2cbbb4322a6cec8536a743ac392c0cd7
Michel Albert <michel@albert.lu>
2019-02-19 09:18:57 +0100

    Remove unnecessary comment

--- code/modules/app.py
+++ code/modules/app.py
@@ -1,7 +1,6 @@
-# This is an ambiguous import but it is good enough for this example.
-#
-# Imports only the name "read_file" from the module, but the whole module will
-# be loaded and interpreted. This helps keeping the local name-space clean.
+# Import only the name "read_file" from the module
+# The whole module will be loaded and interpreted. This helps keeping the local
+# name-space clean.
 from util import read_file
 
 read_file()
